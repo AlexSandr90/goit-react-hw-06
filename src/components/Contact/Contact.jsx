56d453723964b7c9697735f0372e35b56b9a01e1
Oleksandr <gayrsuk@gmail.com>
2024-06-15 15:50:48 +0300

    Initial commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,31 @@
+import classes from './Contact.module.css';
+import { BsFillPersonFill, BsTelephoneFill } from 'react-icons/bs';
+import PropTypes from 'prop-types';
+import CustomButton from '../CustomButton/CustomButton';
+
+const Contact = ({ id, name, number, deletedContact }) => {
+  return (
+    <>
+      <div className={classes.contactInfo}>
+        <div>
+          <BsFillPersonFill />
+          <span>{name}</span>
+        </div>
+        <div>
+          <BsTelephoneFill />
+          <span>{number}</span>
+        </div>
+      </div>
+      <CustomButton buttonText={'Delete'} onClick={() => deletedContact(id)} />
+    </>
+  );
+};
+
+export default Contact;
+
+Contact.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  number: PropTypes.string.isRequired,
+  deletedContact: PropTypes.func.isRequired,
+};
